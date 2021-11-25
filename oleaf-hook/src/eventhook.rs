638a6bcbbfc07467818181851908576585c5980e
SirOlaf <a>
2021-11-25 22:34:33 +0100

    Strange SendEvent hook

---
+++ oleaf-hook/src/eventhook.rs
@@ -0,0 +1,81 @@
+use std::ffi::CStr;
+use std::alloc::{dealloc, Layout};
+use std::ptr;
+
+use libc::c_char;
+use windows::Win32::System::Threading::GetCurrentThreadId;
+use detour::static_detour;
+
+#[repr(C)]
+union StdStrImpl {
+    buf: [c_char; 0x10],
+    ptr: *mut c_char,
+}
+
+#[repr(C)]
+pub struct CxxString {
+    ipl: StdStrImpl,
+    size: i32,
+}
+
+impl CxxString {
+    pub fn new<S: ToString>(data: S) -> Self {
+        let data = data.to_string();
+        if data.len() > 15 {
+            Self { size: data.len() as i32, ipl: StdStrImpl { ptr: Box::into_raw(Box::new(data)) as _ } }
+        } else {
+            let mut buf = [0; 0x10];
+            buf.copy_from_slice(data.as_bytes());
+            Self { size: data.len() as i32, ipl: StdStrImpl { buf: unsafe { std::mem::transmute(buf) } } }
+        }
+    }
+    
+    pub fn c_str<'a>(&self) -> &'a CStr {
+        unsafe { // Real gamers hide unsafety
+            if self.size < 16 {
+                CStr::from_ptr(self.ipl.buf.as_ptr())
+            } else {
+                CStr::from_ptr(self.ipl.ptr)
+            }
+        }
+    }
+}
+
+impl Drop for CxxString {
+    fn drop(&mut self) {
+        if self.size > 15 {
+            unsafe {
+                ptr::drop_in_place(self.ipl.ptr);
+                dealloc(self.ipl.ptr as *mut u8, Layout::new::<String>());
+            }
+        }
+    }
+}
+
+static_detour! {
+    pub static SendEventHook: fn(i64, i64, i64) -> i64;
+}
+
+pub type FnSendEvent = fn(i64, i64, i64) -> i64;
+pub type FnEventHandlerGetter = fn(i64, i64) -> i64;
+
+pub static mut EVENT_HANDLER_GETTER_PTR: *const () = 0 as *mut ();
+
+pub fn send_event_detour(dispatcher: i64, name: i64, unknown: i64) -> i64 {
+    unsafe {
+        let testname = CxxString::new("HandleQuestDialog");
+
+        let handler_addr = std::mem::transmute::<*const (), FnEventHandlerGetter>(EVENT_HANDLER_GETTER_PTR)(dispatcher, *std::mem::transmute::<&CxxString, *const *const ()>(&testname) as i64);
+        if handler_addr != 0 {
+            // We don't need the hook anymore (should do this after all things are found)
+            println!("Found HandleQuestDialog at {:x}", handler_addr);
+
+            match SendEventHook.disable() {
+                Ok(()) => (),
+                Err(err) => println!("{}", err)
+            }
+        }
+        
+        SendEventHook.call(dispatcher, name, unknown)
+    }
+}
--- oleaf/src/lib.rs
+++ oleaf/src/lib.rs
@@ -1,15 +1,43 @@
+use std::error::Error;
+
 use windows::Win32::Foundation::{BOOL, HINSTANCE};
+use windows::Win32::System::SystemServices::{DLL_PROCESS_ATTACH};
+
+use oleaf_hook::module::Module;
+use oleaf_hook::eventhook::*;
 
 #[cfg(not(all(target_arch = "x86_64", target_os = "windows")))]
 compile_error!("Only x64 builds for Windows are supported!");
+
+unsafe fn main() -> Result<(), Box<dyn Error>> {
+    let cur_mod = Module::pe().ok_or("Failed to get the current module")?;
+    let send_event_target: FnSendEvent = std::mem::transmute(cur_mod.find_signature("40 ?? 56 57 41 ?? 41 ?? 41 ?? 41 ?? 48 81 ?? ?? ?? ?? ?? ?? c7 ?? ?? ?? ?? ?? ?? ?? ?? 89 ?? ?? ?? ?? ?? ?? 48 8b ?? ?? ?? ?? ?? 48 33 ?? ?? 89 ?? ?? ?? ?? ?? ?? 4d 8b ?? ?? 89")?);
+
+    EVENT_HANDLER_GETTER_PTR = std::mem::transmute(cur_mod.find_signature("41 56 48 83 EC ?? 48 C7 44 24 20 FE FF FF FF 48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 48 8B FA 4C 8B C9")?);
+    println!("EventHandler getter found at: {:x}", EVENT_HANDLER_GETTER_PTR as i64);
+
+    SendEventHook
+        .initialize(send_event_target, send_event_detour)?
+        .enable()?;
+
+    println!("Hooked SendEvent\n");
+    
+    Ok(())
+}
 
 /// Entrypoint to the oleaf application.
 #[allow(clippy::missing_safety_doc, non_snake_case)]
 #[no_mangle]
 pub unsafe extern "system" fn DllMain(
     _module: HINSTANCE,
-    _call_reason: u32,
+    call_reason: u32,
     _reserved: *const (),
 ) -> BOOL {
-    todo!()
+    BOOL::from(
+        if call_reason == DLL_PROCESS_ATTACH {
+            main().is_ok()
+        } else {
+            true
+        }
+    )
 }
